bb4314f547925d18ac39be27b8e58c40c69bc841
ParvathiPadmeswaran <93865941+ParvathiPadmeswaran@users.noreply.github.com>
2022-12-03 14:22:11 +0530

    Add files via upload

---
+++ login/ulog_action.php
@@ -0,0 +1,18 @@
+<?php
+    include('connection.php');
+    if(isset($_POST['login']))
+{
+	$flag=0;
+	$email=$_POST['your-email'];
+	$pass=$_POST['password'];
+	$res=mysqli_query($con,"select * from registration where email='$email' and password='$pass'");
+	while($row=mysqli_fetch_array($res))
+	{
+		$flag=1;
+	}
+	if($flag==1)
+	{
+		header('location:../home.php');
+	}
+}
+?>
